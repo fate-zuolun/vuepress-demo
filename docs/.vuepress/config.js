ba5fa9326afec7fa56d2d3eca1dd812bf25dfc14
cuicheng <609519711@qq.com>
2022-06-18 17:37:18 +0800

    fix

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  themeConfig: {
+    logo: '/assets/img/icon-bell.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'about', link: '/about' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
