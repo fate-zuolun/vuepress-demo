ac74fa8f119b0caaa125d3f2669831b3af37705b
13725199962 <609519711@qq.com>
2022-06-20 11:19:19 +0800

    添加插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,12 +5,23 @@ module.exports = {
     nav: [
       { text: '主页', link: '/' },
       { text: '关于我', link: '/about/aboutMe' },
+      { text: '我的前端历程', link: '/myWeb/myWeb' },
     ],
     sidebar: {
       '/about/': [
-        ['aboutMe','关于我'],
-        ['aboutindex','aboutindex的东西'],
+        ['aboutMe', '关于我'],
       ],
-    }
-  }
+      '/myWeb/': [
+        ['myWeb', '我的前端历程'],
+      ],
+    },
+  },
+  plugins: [
+    [
+      '@vuepress-reco/vuepress-plugin-kan-ban-niang',
+      {
+        theme: ['blackCat', 'whiteCat', 'haru1', 'haru2', 'haruto', 'koharu', 'izumi', 'shizuku', 'wanko', 'miku', 'z16']
+      }
+    ],
+  ]
 }
---
+++ docs/myWeb/myWeb.md
@@ -0,0 +1,3 @@
+# 我的前端历程
+软件架构说明dwdeweeeeeeeeeeeeeeeeeeee
+*这个是斜体*
